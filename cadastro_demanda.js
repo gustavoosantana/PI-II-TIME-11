bcf62b4608ab4016f284da512856e83f28fcf858
Eduardo Martins Colmati <eduardo.mc2@puccampinas.edu.br>
2026-09-17 09:21:38 -0300

    integração com js

---
+++ cadastro_demanda.js
@@ -0,0 +1,68 @@
+const erro_nome = document.getElementById("erro_nome");
+const erro_desc = document.querySelector("#erro_desc");
+const erro_assoc = document.querySelector("#erro_assoc");
+const erro_tipos = document.querySelector("#erro_tipos")
+const erro_prio = document.querySelector("#erro_prio")
+const erro_status = document.querySelector("#erro_status")
+const erro_prazo = document.querySelector("#erro_prazo")
+const erro_data = document.querySelector("#erro_prazo")
+const campo_nome = document.querySelector(".campo_nome");
+const campo_desc = document.querySelector(".campo_desc")
+const campo_assoc = document.querySelector(".campo_assoc")
+const campo_tipos = document.querySelectorAll('input[name="Tipo"]');
+const campo_prio = document.querySelectorAll('input[name="Prioridade"]');
+const campo_status = document.querySelectorAll('input[name="Status"]');
+const campo_data = document.querySelector(".data")
+const botao = document.querySelector(".botao");
+
+botao.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (campo_nome.value === "") {
+        erro_nome.style.display = "flex"
+    }
+    if (campo_desc.value === "") {
+        erro_desc.style.display = "flex"
+    }
+
+    if (campo_assoc.value === "") {
+        erro_assoc.style.display = "flex"
+    }
+
+    tipo = false
+
+    campo_tipos.forEach((check) => {
+        if(check.checked) {
+            tipo = true
+        }
+    })
+
+    if(!tipo) {
+        erro_tipos.style.display="flex";
+    }
+
+    prio = false
+    campo_prio.forEach((check) => {
+         if(check.checked) {
+            tipo = true
+        }
+    })
+
+    if(!prio) {
+        erro_prio.style.display="flex";
+    }
+
+    statys = false
+    campo_status.forEach((check) => {
+         if(check.checked) {
+            status = true
+        }
+    })
+
+    if(!statys) {
+        erro_status.style.display="flex";
+    }
+
+    if(campo_data.value === "") {
+        erro_data.style.display = "flex";
+    }
+})
