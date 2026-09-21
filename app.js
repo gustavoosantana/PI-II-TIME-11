cbcbc9032e47d6d08807d14eb51440c013a50509
Gabriel Lopes Londe Rodrigues <162214906+Lopesloro@users.noreply.github.com>
2026-09-21 02:15:50 -0300

    Adicionando direcionamento de telas

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from 'express'
 
+// Rotas da tela de Detalhes da Demanda - Gabriel Lopes Londe Rodrigues
+import { rotas as rotasDetalhes } from './detalhes.demanda.rotas.js'
+
 
 const app = express()
+
+// Permite ler o JSON que as telas enviam nas requisicoes
+app.use(express.json())
+
+// Entrega os arquivos HTML, CSS e JS da pasta pages
+app.use(express.static('pages'))
+
+// Liga as rotas da tela de Detalhes da Demanda
+app.use(rotasDetalhes)
 
 app.listen(3000, () => {
     console.log("Servidor aberto!")
