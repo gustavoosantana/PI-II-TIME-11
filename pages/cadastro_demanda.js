6f12da4cdf72b4f5c8c24e91fd7d43d545f030c6
Eduardo Martins Colmati <eduardo.mc2@puccampinas.edu.br>
2026-09-21 11:12:58 -0300

    header cadastro

--- pages/cadastro_demanda.js
+++ pages/cadastro_demanda.js
@@ -1,3 +1,4 @@
+// Seleciona as mensagens de erro e os campos do formulário
 const erro_nome = document.getElementById("erro_nome");
 const erro_desc = document.querySelector("#erro_desc");
 const erro_assoc = document.querySelector("#erro_assoc");
@@ -15,54 +16,69 @@ const campo_status = document.querySelectorAll('input[name="Status"]');
 const campo_data = document.querySelector(".data")
 const botao = document.querySelector(".botao");
 
+// Executa a validação ao clicar no botão de cadastro
 botao.addEventListener("click", (e) => {
     e.preventDefault()
+    // Verifica se os campos de texto obrigatórios foram preenchidos
+
     if (campo_nome.value === "") {
         erro_nome.style.display = "flex"
+        window.scrollTo({           //faz a pagina rolar para cima, ajudando o usúario a perceber o erro
+        top: 0,
+        behavior: "smooth"
+        });
     }
     if (campo_desc.value === "") {
         erro_desc.style.display = "flex"
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        });
     }
 
     if (campo_assoc.value === "") {
         erro_assoc.style.display = "flex"
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        });
     }
 
-    tipo = false
+//     tipo = false
 
-    campo_tipos.forEach((check) => {
-        if(check.checked) {
-            tipo = true
-        }
-    })
+//     campo_tipos.forEach((check) => {
+//         if(check.checked) {
+//             tipo = true
+//         }
+//     })
 
-    if(!tipo) {
-        erro_tipos.style.display="flex";
-    }
+//     if(!tipo) {
+//         erro_tipos.style.display="flex";
+//     }
 
-    prio = false
-    campo_prio.forEach((check) => {
-         if(check.checked) {
-            tipo = true
-        }
-    })
+//     prio = false
+//     campo_prio.forEach((check) => {
+//          if(check.checked) {
+//             tipo = true
+//         }
+//     })
 
-    if(!prio) {
-        erro_prio.style.display="flex";
-    }
+//     if(!prio) {
+//         erro_prio.style.display="flex";
+//     }
 
-    statys = false
-    campo_status.forEach((check) => {
-         if(check.checked) {
-            status = true
-        }
-    })
+//     statys = false
+//     campo_status.forEach((check) => {
+//          if(check.checked) {
+//             status = true
+//         }
+//     })
 
-    if(!statys) {
-        erro_status.style.display="flex";
-    }
+//     if(!statys) {
+//         erro_status.style.display="flex";
+//     }
 
-    if(campo_data.value === "") {
-        erro_data.style.display = "flex";
-    }
+//     if(campo_data.value === "") {
+//         erro_data.style.display = "flex";
+//     }
 })
